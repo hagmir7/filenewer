d3c4e7c02f24e11d1837d5b60672325f01eff477
Agmir Hassane <hagmir6@gmail.com>
2026-03-28 14:04:41 +0100

    modified:   app/Http/Controllers/SitemapController.php

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 use App\Models\Page;
@@ -21,6 +22,16 @@ class SitemapController extends Controller
             $sitemap->add(
                 Url::create("/pages/{$page->slug}")
                     ->setLastModificationDate($page->updated_at)
+                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                    ->setPriority(0.8)
+            );
+        });
+
+
+        Blog::where('is_published', 1)->each(function ($blog) use ($sitemap) {
+            $sitemap->add(
+                Url::create("/blogs/{$blog->slug}")
+                    ->setLastModificationDate($blog->updated_at)
                     ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
                     ->setPriority(0.8)
             );
