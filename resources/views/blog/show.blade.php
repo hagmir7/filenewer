4e121256be0a980171600a4da8daff37c93a69b7
Agmir Hassane <hagmir6@gmail.com>
2026-04-11 20:23:59 +0100

    Update teh Blog page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -131,25 +131,24 @@
                 @endif
             </div>
 
-       <div class="prose prose-lg max-w-4xl mx-auto
-                prose-p:text-gray-300 prose-p:leading-8 prose-hr:m-0
-                prose-headings:font-semibold prose-headings:tracking-tight
-                prose-h2:text-white prose-h2:mt-16 prose-h2:mb-6 prose-h2:border-b prose-h2:border-gray-800 prose-h2:pb-3
-                prose-h3:text-gray-100 prose-h3:mt-10 prose-h3:mb-4
-                prose-h4:text-gray-200 prose-h4:mt-8 prose-h4:mb-3
-                prose-strong:text-white
-                prose-a:text-blue-400 prose-a:no-underline hover:prose-a:underline
-                prose-blockquote:border-l-blue-500 prose-blockquote:text-gray-300 prose-blockquote:bg-gray-900/40 prose-blockquote:py-2 prose-blockquote:px-6 prose-blockquote:rounded-r-lg
-                prose-code:text-blue-300 prose-code:bg-gray-800 prose-code:px-1.5 prose-code:py-1 prose-code:rounded
-                prose-pre:bg-gray-900 prose-pre:border prose-pre:border-gray-800 prose-pre:rounded-xl prose-pre:p-6
-                prose-ol:text-gray-300 prose-ul:text-gray-300
-                prose-li:marker:text-blue-400
-                prose-img:rounded-xl
-                dark:prose-invert">
+        <div class="prose prose-lg max-w-4xl mx-auto
+                        prose-p:text-gray-300 prose-p:leading-8 prose-hr:m-0
+                        prose-headings:font-semibold prose-headings:tracking-tight
+                        prose-h2:text-white prose-h2:mt-16 prose-h2:mb-6 prose-h2:border-b prose-h2:border-gray-800 prose-h2:pb-3
+                        prose-h3:text-gray-100 prose-h3:mt-10 prose-h3:mb-4
+                        prose-h4:text-gray-200 prose-h4:mt-8 prose-h4:mb-3
+                        prose-strong:text-white
+                        prose-a:text-blue-400 prose-a:no-underline hover:prose-a:underline
+                        prose-blockquote:border-l-blue-500 prose-blockquote:text-gray-300 prose-blockquote:bg-gray-900/40 prose-blockquote:py-2 prose-blockquote:px-6 prose-blockquote:rounded-r-lg
+                        prose-code:text-blue-300 prose-code:bg-gray-800 prose-code:px-1.5 prose-code:py-1 prose-code:rounded
+                        prose-pre:bg-gray-900 prose-pre:border prose-pre:border-gray-800 prose-pre:rounded-xl prose-pre:p-6
+                        prose-ol:text-gray-300 prose-ul:text-gray-300
+                        prose-li:marker:text-blue-400
+                        prose-img:rounded-xl
+                        dark:prose-invert">
 
-        {!! $blog->content !!}
-
-    </div>
+                {!! $blog->content !!}
+            </div>
 
             <!-- Tags -->
             @if($blog->category)
